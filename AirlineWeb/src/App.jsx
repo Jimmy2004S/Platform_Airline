03c70da00e14755a9947bdb8670c3a3a36d89bfe
Leidi <leididelapuente3@gmail.com>
2023-08-10 23:43:35 -0500

    Add flight method

--- AirlineWeb/src/App.jsx
+++ AirlineWeb/src/App.jsx
@@ -1,17 +1,36 @@
 import Header from "./components/Header";
 import FlightCard from "./components/FlightCard";
 import "./css/global.css";
+import { useState } from "react";
+import { FormAddFlight } from "./components/Form";
 
 function App() {
-    return (
-        <>
-            <Header />
-            <img src="../../public/icon.jpg" alt="" />
-            <div className="grid-container">
-                <FlightCard />
-            </div>
-        </>
-    );
+  const [showForm, setshowForm] = useState(false);
+
+  const openForm = () => {
+    setshowForm(true);
+  };
+
+  const closeForm = () => {
+    setshowForm(false);
+  };
+  return (
+    <>
+      <Header />
+      <button className="btn-add" onClick={openForm}>
+        Add Flight
+      </button>
+      <div className="grid-container">
+        <FlightCard />
+      </div>
+      {showForm && (
+        <div>
+          <div className="overlay" onClick={closeForm}></div>
+          <FormAddFlight onClose={closeForm} />
+        </div>
+      )}
+    </>
+  );
 }
 
 export default App;
